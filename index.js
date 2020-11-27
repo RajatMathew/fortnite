7ea1fdcd7d80f8fc3c18889cc8ea2d6434586351
Rajat Mathew <rajatxmathew@gmail.com>
2020-11-27 18:38:44 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let lion = document.getElementById('lion');
+let blue = document.getElementById('blue');
+let girl = document.getElementById('girl');
+let fortnite = document.getElementById('fortnite');
+
+window.addEventListener('scroll', function ()
+{
+    var value = window.scrollY;
+
+    lion.style.top = value * 0.5 + 'px';
+    blue.style.top = value * 0.5 + 'px';
+    girl.style.top = value * 0.25 + 'px';
+    fortnite.style.top = value * 0.15 + 'px';
+})
